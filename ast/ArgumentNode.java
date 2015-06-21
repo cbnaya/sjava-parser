f8d2678c2b733881f44c96f18264da9a46d5383a
bnaya <cbnaya@gmail.com>
2015-06-21 16:12:36 +0300

    make ArgumentNode inherit from VarDeclarationNode

--- ast/ArgumentNode.java
+++ ast/ArgumentNode.java
@@ -5,28 +5,9 @@ import lexer.Position;
 /**
  * Created by cbnaya on 14/06/2015.
  */
-public class ArgumentNode extends AstNode {
+public class ArgumentNode extends VarDeclarationNode {
     public ArgumentNode(Position position, String argType, String argName, boolean isFinal) {
-        super(position);
-        type = ExpressionNode.ExpressionType.valueOf(argType.toUpperCase());
-        name = argName;
-        this.isFinal = isFinal;
-    }
-
-    private ExpressionNode.ExpressionType type;
-    private String name;
-    private boolean isFinal;
-
-    public ExpressionNode.ExpressionType getType() {
-        return type;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public boolean isFinal() {
-        return isFinal;
+        super(position, argType, argName, isFinal);
     }
 
     @Override
