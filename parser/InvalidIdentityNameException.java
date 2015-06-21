fcefc65695dda3a3e5bd77a4b67bdc7b882fcf59
bnaya <cbnaya@gmail.com>
2015-06-21 18:15:52 +0300

    rename the parser exceptions to the convention and make inherit from Exception (instead of Throwable)

--- parser/InvalidIdentityNameException.java
+++ parser/InvalidIdentityNameException.java
@@ -5,8 +5,8 @@ import lexer.Token;
 /**
  * Created by cbnaya on 15/06/2015.
  */
-public class InvalidIdentityName extends Throwable {
-    public InvalidIdentityName(Token tok) {
+public class InvalidIdentityNameException extends Exception {
+    public InvalidIdentityNameException(Token tok) {
         super(String.format("error (%s) invalid var name: %s ",
                 tok.getStartPosition().toString(),tok.getData()));
     }
--- parser/OtherTokenTypeNeedHereException.java
+++ parser/OtherTokenTypeNeedHereException.java
@@ -2,8 +2,8 @@ package parser;
 
 import lexer.Token;
 
-public class OtherTokenTypeNeedHere extends Throwable {
-    public OtherTokenTypeNeedHere(Token tok, Token.TokenType type)
+public class OtherTokenTypeNeedHereException extends Exception {
+    public OtherTokenTypeNeedHereException(Token tok, Token.TokenType type)
     {
         super(String.format("error (%s) need here token of type %s instead of %s (%s)",
                 tok.getStartPosition().toString(), type.name(),tok.getType().name(),tok.getData()));
