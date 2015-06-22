100d547d9dc97bbc1ef5b90e7465fc0dc85fd59b
NoamGeller <noam.geller@mail.huji.ac.il>
2015-06-22 16:03:39 +0300

    javadoc-ed and fixed stuff in VarStack and Variable.

--- oop/ex6/validator/VarStack.java
+++ oop/ex6/validator/VarStack.java
@@ -8,68 +8,103 @@ import oop.ex6.lexer.Position;
 import java.util.ArrayList;
 
 /**
- * Created by cbnaya on 19/06/2015.
+ * Keeps the state of all variables for one scope at a time.
  */
 public class VarStack {
-    public VarStack()
-    {
-        stack = new ArrayList<ArrayList<Variable>>();
-    }
 
-    public void enterScope()
-    {
-        stack.add(new ArrayList<Variable>());
-    }
+	ArrayList<ArrayList<Variable>> stack;
 
-    public void exitScope()
-    {
-        stack.remove(stack.size()-1);
-    }
+	/**
+	 * Constructs a new VarStack.
+	 */
+	public VarStack() {
+		stack = new ArrayList<ArrayList<Variable>>();
+	}
 
-    public Variable add(VarDeclarationNode varDeclarationNode) throws VarDuplicateDeclaration {
-        if (null != getFromLevel(stack.size()-1, varDeclarationNode.getName()))
-        {
-            throw new VarDuplicateDeclaration(varDeclarationNode);
-        }
-        Variable var = new Variable(varDeclarationNode.getName(), varDeclarationNode.getType(),
-                varDeclarationNode.isFinal());
-        stack.get(stack.size()-1).add(var);
-        return var;
-    }
+	/**
+	 * Adds a new variable to the stack.
+	 * @param varDeclarationNode The declaration of that variable.
+	 * @return The new Variable object. 
+	 * @throws VarDuplicateDeclaration If a variable with the same name was
+	 * alrady declared at the same scope.
+	 */
+	public Variable add(VarDeclarationNode varDeclarationNode) throws 
+			VarDuplicateDeclaration {
+		if (null != getFromLevel(stack.size()-1, varDeclarationNode.getName()))
+		{
+			throw new VarDuplicateDeclaration(varDeclarationNode);
+		}
+		Variable var = new Variable(varDeclarationNode.getName(), 
+				varDeclarationNode.getType(), varDeclarationNode.isFinal());
+		stack.get(stack.size()-1).add(var);
+		return var;
+	}
 
-    private Variable getFromLevel(int level, String name)
-    {
-        for(Variable var:stack.get(level))
-        {
-            if (var.getName().equals(name))
-            {
-                return var;
-            }
-        }
+	/**
+	 * Enters a new code scope.
+	 */
+	public void enterScope() {
+		stack.add(new ArrayList<Variable>());
+	}
 
-        return null;
-    }
+	/**
+	 * Exits the current code scope, and forgets all its variables.
+	 */
+	public void exitScope() {
+		stack.remove(stack.size()-1);
+	}
+	
+	/*
+	 * Searches for a variable at a specific scope.
+	 * @param level The level of the inspected scope. The global level is 0.
+	 * @param name The name of the searched var.
+	 * @return The searched var.
+	 */
+	private Variable getFromLevel(int level, String name) {
+		for(Variable var:stack.get(level)) {
+			if (var.getName().equals(name)) {
+				return var;
+			}
+		}
 
-    private Variable get(String name, Position pos) throws VarDoesNotExistException {
-        for (int i = stack.size() - 1; i >= 0; i--) {
-            Variable var = getFromLevel(i, name);
-            if (null != var) {
-                return var;
-            }
-        }
+		return null;
+	}
 
-        throw new VarDoesNotExistException(name, pos);
-    }
+	/*
+	 * @param name A name of required variable.
+	 * @param pos In case something will go wrong.
+	 * @return The variable.
+	 * @throws VarDoesNotExistException If this variable was never declared.
+	 */
+	private Variable get(String name, Position pos) 
+			throws VarDoesNotExistException {
+		for (int i = stack.size() - 1; i >= 0; i--) {
+			Variable var = getFromLevel(i, name);
+			if (null != var) {
+				return var;
+			}
+		}
+		throw new VarDoesNotExistException(name, pos);
+	}
+	
+	/**
+	 * @param assignmentNode An ast node representing an assignment.
+	 * @return The variable assigning to at the given node.
+	 * @throws VarDoesNotExistException If this variable was never declared.
+	 */
+	public Variable get(AssignmentNode assignmentNode) 
+			throws VarDoesNotExistException {
+		return get(assignmentNode.getName(), assignmentNode.getPosition());
+	}
 
-
-
-    public Variable get(AssignmentNode assignmentNode) throws VarDoesNotExistException {
-        return get(assignmentNode.getName(), assignmentNode.getPosition());
-    }
-
-    public Variable get(VarExpressionNode varExpressionNode) throws VarDoesNotExistException {
-        return get(varExpressionNode.getName(), varExpressionNode.getPosition());
-    }
-
-    ArrayList<ArrayList<Variable>> stack;
+	/**
+	 * @param varExpressionNode An ast node representing a variable expression.
+	 * @return The referenced variable.
+	 * @throws VarDoesNotExistException If this variable was never declared.
+	 */
+	public Variable get(VarExpressionNode varExpressionNode) 
+			throws VarDoesNotExistException {
+		return get(varExpressionNode.getName(), 
+				varExpressionNode.getPosition());
+	}
 }
--- oop/ex6/validator/Variable.java
+++ oop/ex6/validator/Variable.java
@@ -1,40 +1,68 @@
 package oop.ex6.validator;
 
-import oop.ex6.ast.ExpressionNode;
+import oop.ex6.ast.ExpressionNode.ExpressionType;
 
+/**
+ * Keeps the state of a single variable in the program.
+ */
 public class Variable {
 
 	private final String name;
-	private final ExpressionNode.ExpressionType type;
-	private boolean hasValue, isFinal;
+	private final ExpressionType type;
+	private boolean hasValue;
+	private final boolean isFinal;
 	
-	public Variable(String name, ExpressionNode.ExpressionType type, boolean isFinal) {
+	/**
+	 * @param name The variable's name.
+	 * @param type The variable's type.
+	 * @param isFinal Whether the variable is assignable after declaration.
+	 */
+	public Variable(final String name, final ExpressionType type, 
+			final boolean isFinal) {
 		this.name = name;
 		this.type = type;
-		hasValue = false;
 		this.isFinal = isFinal;
+		hasValue = false;
 	}
 	
-	public boolean assign(ExpressionNode.ExpressionType valueType) {
-        if (type.accept(valueType) && (!hasValue || !isFinal)) {
+	/**
+	 * Marks this variable as initialized with a value, if it's not final or the
+	 * type does not match.
+	 * @param valueType The type of the assigned expression.
+	 * @return Whether the assign succeeded.
+	 */
+	public boolean assign(final ExpressionType valueType) {
+        if (type.accept(valueType) && !(hasValue && isFinal)) {
             hasValue = true;
             return true;
         }
         return false;
     }
 
+	/**
+	 * @return The variable's name.
+	 */
 	public String getName() {
 		return name;
 	}
 	
-	public ExpressionNode.ExpressionType getType() {
+	/**
+	 * @return The variable's type.
+	 */
+	public ExpressionType getType() {
 		return type;
 	}
 	
+	/**
+	 * @return Whether the variable was assigned.
+	 */
 	public boolean hasValue() {
 		return hasValue;
 	}
 	
+	/**
+	 * @return Whether the variable is assignable after declaration.
+	 */
 	public boolean isFinal() {
 		return isFinal;
 	}
